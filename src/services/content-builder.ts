398e9c4ff1b3626ce49337edfead9297215ea443
Temitope Adeyeri <temi.adeyeri@gmail.com>
2017-07-27 22:50:50 -0500

    .

--- src/services/content-builder.ts
+++ src/services/content-builder.ts
@@ -1,15 +1,15 @@
-import { Theme } from './../theme';
-import { Page } from './../page';
-import { SiteApp } from './../site-app';
+import { Theme } from "./../theme";
+import { Page } from "./../page";
+import { SiteApp } from "./../site-app";
 import { ContentData } from "./../content-data";
 import { Widget } from "../widget";
 
 export class ContentBuilder {
   constructor() {}
   static buildPreview(data: ContentData) {
-    data.site_info = data.site_info || new SiteApp;
-    data.page = data.page || new Page;
-    data.theme = data.theme || new Theme;
+    data.site_info = data.site_info || new SiteApp();
+    data.page = data.page || new Page();
+    data.theme = data.theme || new Theme();
 
     data.theme.styles = data.theme.styles || [];
     data.page.styles = data.page.styles || [];
@@ -26,7 +26,9 @@ export class ContentBuilder {
       data.theme.styles.concat(data.page.styles)
     );
     const metadata = ContentBuilder.buildTagElements(
-      data.theme.metadata.concat(data.page.metadata).concat(data.site_info.metadata)
+      data.theme.metadata
+        .concat(data.page.metadata)
+        .concat(data.site_info.metadata)
     );
     const scripts = ContentBuilder.buildTagElements(
       data.theme.scripts.concat(data.page.scripts)
@@ -47,7 +49,7 @@ export class ContentBuilder {
     return content;
   }
 
-  static  buildAttributes(attributes) {
+  static buildAttributes(attributes) {
     attributes = attributes || [];
     let str = "";
     attributes.forEach(attr => {
@@ -57,10 +59,12 @@ export class ContentBuilder {
     });
     return str;
   }
-  static   buildTagElements(tags) {
+  static buildTagElements(tags) {
     let tagString = "";
     tags.forEach(item => {
-      let str = `<${item.tag_name} ${ContentBuilder.buildAttributes(item.attributes)}>`;
+      let str = `<${item.tag_name} ${ContentBuilder.buildAttributes(
+        item.attributes
+      )}>`;
       if (!item.self_closing || item.content) {
         str += `${item.content}</${item.tag_name}>`;
       }
@@ -68,7 +72,7 @@ export class ContentBuilder {
     });
     return tagString;
   }
-  static   buildWidgetGridAttributes(widget) {
+  static buildWidgetGridAttributes(widget) {
     widget = widget || {};
     const def = widget.grid_definition;
     const keys = Object.keys(def);
@@ -81,10 +85,11 @@ export class ContentBuilder {
     return gridClasses;
   }
 
-  static  buildWidgets(widgets: Widget[]) {
+  static buildWidgets(widgets: Widget[]) {
     widgets = widgets || [];
     let content = ``;
     widgets.forEach(widget => {
+      widget.tag_name = widget.tag_name || "div";
       let attr = ContentBuilder.buildAttributes(widget.attributes);
       if (!attr.includes('class="')) {
         attr += 'class=""';
