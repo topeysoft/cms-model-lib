b3edfedb7691b4d9e1969d090ec9e922d7525a92
Temi Adeyeri <temi.adeyeri@gmail.com>
2017-08-02 13:10:27 -0500

    code update

---
+++ build/widget-definition.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class WidgetDefinition {
+}
+exports.WidgetDefinition = WidgetDefinition;
+//# sourceMappingURL=widget-definition.js.map
