e74a33b87aea056d07809a8c251f5769f1fce74f
Temi Adeyeri <temi.adeyeri@gmail.com>
2017-08-02 13:02:01 -0500

    .

--- build/sub-models/grid-definition-item.js
+++ build/sub-models/grid-definition-item.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class GridDefinitionItem {
     constructor() {
         this.size = 12;
-        this.hidden = true;
+        this.hidden = false;
     }
 }
 exports.GridDefinitionItem = GridDefinitionItem;
--- build/sub-models/grid-definition.js
+++ build/sub-models/grid-definition.js
@@ -1,6 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const grid_definition_item_1 = require("./grid-definition-item");
 class GridDefinition {
+    constructor() {
+        this.xs = new grid_definition_item_1.GridDefinitionItem;
+        this.sm = new grid_definition_item_1.GridDefinitionItem;
+        this.md = new grid_definition_item_1.GridDefinitionItem;
+        this.lg = new grid_definition_item_1.GridDefinitionItem;
+        this.xl = new grid_definition_item_1.GridDefinitionItem;
+        this.pt = new grid_definition_item_1.GridDefinitionItem;
+        this.xl = new grid_definition_item_1.GridDefinitionItem;
+        this.lg = new grid_definition_item_1.GridDefinitionItem;
+        this.md = new grid_definition_item_1.GridDefinitionItem;
+        this.sm = new grid_definition_item_1.GridDefinitionItem;
+        this.xs = new grid_definition_item_1.GridDefinitionItem;
+        this.pt = new grid_definition_item_1.GridDefinitionItem;
+    }
 }
 exports.GridDefinition = GridDefinition;
 //# sourceMappingURL=grid-definition.js.map
