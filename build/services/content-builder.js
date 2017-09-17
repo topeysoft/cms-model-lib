35cec7effabd4d5817b7be3e9b95b5d37d8d0bc3
Adeyeri <adeyeri@Adeyeris-MacBook-Air.local>
2017-09-16 19:01:30 -0500

    code update

--- build/services/content-builder.js
+++ build/services/content-builder.js
@@ -19,6 +19,10 @@ class ContentBuilder {
         contentData.theme.styles = contentData.theme.styles || [];
         contentData.theme.scripts = contentData.theme.scripts || [];
         contentData.theme.metadata = contentData.theme.metadata || [];
+        const project_id_global_script = new index_1.ScriptElement;
+        project_id_global_script.tag_name = 'script';
+        project_id_global_script.content = `const project_id = '${contentData.site_info._id}';`;
+        contentData.theme.scripts.splice(0, 0, project_id_global_script);
         let data = Object.assign({}, contentData);
         if (fromDraft) {
             data.page = contentData.page.draft || new page_1.Page();
--- src/services/content-builder.ts
+++ src/services/content-builder.ts
@@ -23,6 +23,11 @@ export class ContentBuilder {
     contentData.theme.scripts = contentData.theme.scripts || [];
     contentData.theme.metadata = contentData.theme.metadata || [];
 
+      const project_id_global_script = new ScriptElement;
+      project_id_global_script.tag_name = 'script';
+      project_id_global_script.content = `const project_id = '${contentData.site_info._id}';`;
+      contentData.theme.scripts.splice(0, 0 ,project_id_global_script);
+
     let data = Object.assign({}, contentData);
     if (fromDraft) {
       data.page = contentData.page.draft || new Page();
