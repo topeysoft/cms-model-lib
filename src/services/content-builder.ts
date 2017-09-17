574fb175e6d922215eb6e28abb8ac6438880dd86
Adeyeri <adeyeri@Adeyeris-MacBook-Air.local>
2017-09-17 12:57:57 -0500

    code update

--- src/services/content-builder.ts
+++ src/services/content-builder.ts
@@ -29,7 +29,7 @@ export class ContentBuilder {
         {enabled:true, key:'type', value:'text/javascript'}
       ];
       project_id_global_script.render_in_head = true;
-      project_id_global_script.content = `let project_id = '${contentData.site_info._id}';`;
+      project_id_global_script.content = `window.project_id = window.project_id || '${contentData.site_info._id}';`;
       contentData.theme.scripts.splice(0, 0 ,project_id_global_script);
 
     let data = Object.assign({}, contentData);
